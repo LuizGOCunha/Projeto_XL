c247b0bdd3e0356b9bd68c581714250bc73ec099
Luiz Oliveira <valuiz1@gmail.com>
2022-01-12 12:22:08 +0000

    Esqueleto do front-end estabelecido, ainda precisa de um header

--- XL_website/main_pages/urls.py
+++ XL_website/main_pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('sobre/', views.sobre, name='sobre'),
     path('contato/', views.contato, name='contato'),
     path('projetos/', views.projetos, name='projetos'),
