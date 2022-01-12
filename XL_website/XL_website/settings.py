afad62b1250051c48c1d69247849eed87ccdd7d8
Luiz Oliveira <valuiz1@gmail.com>
2022-01-12 18:16:33 +0000

    Esqueleto da página principal estabelecido

--- XL_website/XL_website/settings.py
+++ XL_website/XL_website/settings.py
@@ -119,11 +119,15 @@ USE_TZ = True
 
 STATIC_URL = 'template/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static/')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static/')
-]
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(STATICFILES_DIRS[0], 'media')
--- XL_website/main_pages/urls.py
+++ XL_website/main_pages/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from. import views
 
@@ -6,4 +8,4 @@ urlpatterns = [
     path('sobre/', views.sobre, name='sobre'),
     path('contato/', views.contato, name='contato'),
     path('projetos/', views.projetos, name='projetos'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
